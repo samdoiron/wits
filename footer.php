175bf149184b24d6c7c96d782ba5a2f2f8a58c03
Gilberto De Melo <gilberto@innetwork.net>
2014-09-24 19:28:15 -0300

    Finished navbar. Also included favicon.

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div id="footer">
 	<div class="container">
-		<p class="text-muted">With <span class="glyphicon glyphicon-heart"></span> from Web Development Society</p>
+		<p class="text-muted">With <span class="glyphicon glyphicon-heart"></span> </p>
 	</div>
 </div>
