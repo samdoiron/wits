a0f17517e53b3328731e57deba71842a45494bc9
andreykul <andrey.kulakevich@gmail.com>
2014-10-01 18:29:19 -0300

    modified some of the names for links in the navbar.

--- navbar.php
+++ navbar.php
@@ -13,9 +13,9 @@
 		<div class="collapse navbar-collapse">
 			<ul class="nav navbar-nav">
 				<li <?php if (isset($home)) echo 'class="active"'; ?>><a href="index.php">Home</a></li>
-				<li <?php if (isset($about)) echo 'class="active"'; ?>><a href="news.php">News</a></li>
-				<li <?php if (isset($portfolio)) echo 'class="active"'; ?>><a href="minutes.php">Meeting Minutes</a></li>
-				<li <?php if (isset($members)) echo 'class="active"'; ?>><a href="members.php">Event </a></li>
+				<li <?php if (isset($news)) echo 'class="active"'; ?>><a href="news.php">News</a></li>
+				<li <?php if (isset($minutes_page)) echo 'class="active"'; ?>><a href="minutes.php">Meeting Minutes</a></li>
+				<li <?php if (isset($events)) echo 'class="active"'; ?>><a href="events.php">Events</a></li>
 				<li <?php if (isset($members)) echo 'class="active"'; ?>><a href="members.php">Members</a></li>
 				<li <?php if (isset($contact)) echo 'class="active"'; ?>><a href="#">Contact</a></li>
 			</ul>
