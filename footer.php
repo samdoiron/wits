425d1c494b9725582845a583b732de235cc96f41
Sam Doiron <samdoiron@fastmail.fm>
2014-10-01 19:02:04 -0300

    Add basic front page

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
-<div id="footer">
-	<div class="container">
-		<p class="text-muted">With <span class="glyphicon glyphicon-heart"></span> from Web Development Society</p>
-	</div>
-</div>
+<footer class="site-footer">
+	yo
+</footer>
+
+<aside class="copyright">
+	<p class="text-muted">With <span class="glyphicon glyphicon-heart"></span> from Web Development Society</p>
+</aside>
--- head.php
+++ head.php
@@ -15,6 +15,9 @@
 	<link href="css/ekko-lightbox.css" rel="stylesheet">
 	<link href="css/custom.css" rel="stylesheet">
 
+	<!-- Web fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,100,500' rel='stylesheet' type='text/css'>
+
 	<!-- Just for debugging purposes. Don't actually copy this line! -->
 	<!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
 
